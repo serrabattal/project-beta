50c2b1a227bb2064c711f2e528730084a0801c70
Cristina Pitu <office@cristinapitu.com>
2021-07-06 15:51:34 -0400

    Defined the Stocks' model

--- mandatory_libraries.py
+++ mandatory_libraries.py
@@ -1,4 +1,22 @@
 import pandas as pd
+from yahoo_fin.stock_info import get_data
+from datetime import date
+import numpy as np
+import seaborn as sns
+from datetime import date
+from datetime import timedelta
+import os
+from io import BytesIO
+import requests
+from twilio.rest import Client
+from dotenv import load_dotenv
+import panel as pn
+import subprocess
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import plotly.express as px
+from MCForecastTools import MCSimulation
 from pathlib import Path
-import yfinance
-from yahoo_fin.stock_info import 
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
